2c6b17b21a9258e9672058bd01a089b8260e640b
Stefan Huynh <huynh.stefan@gmail.com>
2016-10-30 22:48:27 -0700

    Make file changes conditional in teardown_zsh.sh

--- bin/zsh/teardown_zsh.sh
+++ bin/zsh/teardown_zsh.sh
@@ -6,8 +6,12 @@
 main()
 {
   print 'Removing zsh as default shell...'
-  grep -v "^$zsh_path" $shells_path > shells_temp
-  sudo mv shells_temp $shells_path
+
+  if grep -q $zsh_path $shells_path ; then
+    grep -v "^$zsh_path" $shells_path > shells_temp
+    sudo -s mv shells_temp $shells_path
+  fi
+
   chsh -s $default_bash_path
 }
 

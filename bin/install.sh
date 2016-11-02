bc693f345f501682d88e50537244ea6a868db061
Stefan Huynh <huynh.stefan@gmail.com>
2016-11-02 01:06:26 -0700

    Finish install and uninstall scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 . ./bin/utils.sh
+. ./bin/vars.sh
 
 main()
 {
   print 'Installing dotfiles...'
+  bash "$bin_path/homebrew/install.sh"
+  bash "$bin_path/vim/install.sh"
+  bash "$bin_path/tmux/install.sh"
+  bash "$bin_path/zsh/install.sh"
 }
 
 main
--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 . ./bin/utils.sh
+. ./bin/vars.sh
 
 main()
 {
   print 'Uninstalling dotfiles...'
+  bash "$bin_path/homebrew/uninstall.sh"
+  bash "$bin_path/vim/uninstall.sh"
+  bash "$bin_path/tmux/uninstall.sh"
+  bash "$bin_path/zsh/uninstall.sh"
 }
 
 main

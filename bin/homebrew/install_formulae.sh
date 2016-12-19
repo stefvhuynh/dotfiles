b125c7cf6482d80f8aaef7bfd5cb13265257a684
Stefan Huynh <huynh.stefan@gmail.com>
2016-12-19 14:33:12 -0700

    Add mongodb to brew install

--- bin/homebrew/install_formulae.sh
+++ bin/homebrew/install_formulae.sh
@@ -3,7 +3,7 @@
 . ./bin/utils.sh
 
 # Yarn installs node for you. Mercurial is necessary for vim.
-formulae=(zsh git tmux mercurial vim yarn ack)
+formulae=(zsh git tmux mercurial vim yarn ack mongodb)
 
 main()
 {
